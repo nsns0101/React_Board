a3f91b7d2fb270174f72ca664039f79160d1ab2d
nsns0101 <nsns0101@naver.com>
2020-09-03 11:07:48 +0900

    Header Design

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter } from "react-router-dom";
+
+import Header from "../layout/Header";
+// import Footer from "../layout/Footer";
 
 function App() {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">App Component</div>
-
-                        <div className="card-body">I'm an App component!</div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <BrowserRouter>
+            <Header/>
+            {/* <Route path="/" exact={true} component={Home}/>
+            <Route path="/auth" component={Login}/>
+            <Route path="/service" exact={true} component={Service}/>
+            <Route path="/service/create" component={Create}/>     
+            <Route path="/service/response" component={Response}/> 
+            <Route path="/market" component={Market}/>    */}
+        </BrowserRouter>
     );
 }
 
---
+++ resources/js/layout/Header.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+export default () => {
+    return (
+        <div className="body-inner">
+		    <header id="header" className="header-one">
+                <nav className="site-navigation navigation navdown">
+                    <div className="row justify-content-center">
+                        <div className="col-xl-10 col-lg-10 col-md-10 col-sm-10">
+							<div className="site-nav-inner">
+								<button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+									<span className="sr-only">Toggle navigation</span>
+									<span className="icon-bar"></span>
+									<span className="icon-bar"></span>
+									<span className="icon-bar"></span>
+								</button>
+
+								<div className="collapse navbar-collapse navbar-responsive-collapse">
+                                    <ul className="nav navbar-nav">
+                                        <Link to="/">
+                                            <img src="/icon/title2.png" style={{width:"200px", marginBottom:"3px", marginRight:"80px"}}/>
+                                        </Link>
+                                        <li><Link style={{fontSize:"18px", padding:"12px"}} to="/">Home</Link></li>
+                                        <li><Link style={{fontSize:"18px", padding:"12px"}} to="/board">Board</Link></li>
+
+                                        {/*  */}
+                                    </ul>
+                                    <ul className="nav navbar-nav" style={{listStyle:"none", float:"right"}}>
+                                        <li><Link style={{fontSize:"20px", padding:"12px"}} to="/auth/login">Login</Link></li>
+                                    </ul>
+								</div>
+
+							</div>
+
+						</div>
+                        
+                     </div>
+                </nav>
+            </header>
+        </div>
+    )
+}
